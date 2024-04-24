106671aaa070bd32bc82c9e6b73994625e3d0f22
Craig John <craigejohn1@gmail.com>
2024-04-24 11:46:56 -0400

    Added Deadline Message

--- UI.gd
+++ UI.gd
@@ -4,7 +4,13 @@ func update_fuel(value):
 	$FuelBar.value = value
 func update_level_ui(value):
 	$Level.text = str("Level: ", value)
+	$Deadline.show()
+	$Deadline.get_child(0).play("color_change")
 
 func update_fps(fps):
 	var fps_display = snapped(randf_range((fps-1.5), fps+.5), .01)
 	$FPS.text = str("FPS:", fps_display)
+
+
+func _on_animation_player_animation_finished(anim_name):
+	$Deadline.hide()
